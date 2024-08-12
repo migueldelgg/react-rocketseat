abfe419390fc8ad0a4daa38e169939d10fa3df6c
Miguel <delgdevoleper@gmail.com>
2024-08-11 23:14:35 -0300

    Global CSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
 
-import './styles.css'
+import './global.css'
 
 export function App() {
   return (
     <div>
       <Header/>
-
       <Post author = "Miguel" content = "conteudo da pagina"/> 
 
       <Post author = "Gabriel" content = "Post do Gabriel"/> 
