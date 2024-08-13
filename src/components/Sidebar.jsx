9fc7b36eeed28066492b2d5a585c0f6fd2540716
Miguel <delgdevoleper@gmail.com>
2024-08-13 14:02:37 -0300

    Finalizando sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,25 @@
 import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react';
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
             <img
                 className={styles.cover}
-                src="https://images.unsplash.com/photo-1722641277067-a7fba0ad1a59?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" 
+                src="https://images.unsplash.com/photo-1722641277067-a7fba0ad1a59?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/150628051?v=4" />
+
                 <strong>Miguel Delgado</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="https://github.com/migueldelgg">
-                    Github
+                    <PencilLine size={20}/>
+                    Editar seu perfil
                 </a>
             </footer>
         </aside>
