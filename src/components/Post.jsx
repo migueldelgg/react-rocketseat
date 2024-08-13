d0479d1a03d2d72dd3bcec999e9eb11ee94b9687
Miguel <delgdevoleper@gmail.com>
2024-08-13 17:31:12 -0300

    Estilizando componente Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,6 +3,7 @@ import styles from './Post.module.css'
 export function Post() {
     return (
         <article className={styles.post}>
+
             <header>
                 <div className={styles.author}>
                     <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/150628051?v=4" />
@@ -12,14 +13,14 @@ export function Post() {
                     </div>
                 </div>
 
-                <time title='13 de Agosto às 16:37' dateTime='2024-08-13 16:37:00'>Publicado há 1h</time>
+                <time title="13 de Agosto às 16:37" dateTime="2024-08-13 16:37:00">Publicado há 1h</time>
             </header>
 
             <div className={styles.content}>
                 <p>Fala galeraa 👋</p>
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
                 <p>👉 <a href="">jane.design/doctorcare</a></p>
-                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+                <p><a href="">#novoprojeto</a> <a href="">#nlw</a> <a href="">#rocketseat</a></p>
             </div>
         </article>
     );
