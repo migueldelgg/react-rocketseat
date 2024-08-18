0cb3d5c47eb5238c7174f57f834745a41926e0da
Miguel <delgdevoleper@gmail.com>
2024-08-18 14:53:14 -0300

    Inserindo comentários (Programação declarativa), exemplo de programcao declarativa

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -12,7 +12,10 @@ import { useState } from 'react';
 export function Post({author, publishedAt, content}) {
 
     const [comments, setComments] = useState([
+        'Post muito bacana!'
     ])
+
+    const [newCommentText, setNewCommentText] = useState('')
 
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR
@@ -26,9 +29,12 @@ export function Post({author, publishedAt, content}) {
     function handleCreateNewComment() {
         event.preventDefault()
 
-        // imutabilidade
+        setComments([...comments, newCommentText])
+        setNewCommentText('');
+    }
 
-        setComments([...comments, comments.length + 1])
+    function handleNewCommentChange() {
+        setNewCommentText(event.target.value)
     }
 
     return (
@@ -68,7 +74,10 @@ export function Post({author, publishedAt, content}) {
                 <strong>Deixe seu feedback:</strong>
 
                 <textarea
+                    name="comment"
                     placeholder='Escreva um comentário...'
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 />
 
                 <footer>
@@ -78,7 +87,7 @@ export function Post({author, publishedAt, content}) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment/>
+                    return <Comment content={comment}/>
                 })}
             </div>
         </article>
