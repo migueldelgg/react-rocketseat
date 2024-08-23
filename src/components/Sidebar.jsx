a6a7f1382385b792a41263a4b8a3a7d68ad9df66
Miguel <delgdevoleper@gmail.com>
2024-08-22 22:51:41 -0300

    side bar]

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -22,7 +22,7 @@ export function Sidebar() {
             <footer>
                 <a href="https://github.com/migueldelgg">
                     <PencilLine size={20}/>
-                    Editar seu perfil
+                    Editar perfil
                 </a>
             </footer>
         </aside>
