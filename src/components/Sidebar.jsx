0c7256bd9b032f5f5e8cb71256ff2d415a380ac8
Miguel <delgdevoleper@gmail.com>
2024-08-23 22:24:46 -0300

    without img

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ export function Sidebar() {
         <aside className={styles.sidebar}>
             <img
                 className={styles.cover}
-                src="https://images.unsplash.com/photo-1722641277067-a7fba0ad1a59?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""
+                src="" alt=""
             />
 
             <div className={styles.profile}>
